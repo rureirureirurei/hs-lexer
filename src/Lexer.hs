4052b3faed64c862229dd4391345505609e18896
Denys Zinoviev <reiisbetterthanasuka@gmail.com>
2025-02-05 22:20:12 +0100

    Moved regex to nfa translation to the Regex module

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -2,94 +2,12 @@
 
 module Lexer (main) where
 
-import Control.Monad.State
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import Control.Applicative ((<|>))
 import Regex
 import NFA
 import qualified Graph (showNFA)
-
-
--- Auxillary function that merges two NFA's and adds new edges
-mergeNFAs :: Node -> [Node] -> [(Node, [(Transition, Node)])] -> [NFA] -> NFA 
-mergeNFAs start terminals newEdges nfas = 
- mkNfa 
-    start 
-    terminals 
-    (foldl joinTs Map.empty ((Map.fromList newEdges) : (map transitions nfas)))
-
-
--- Auxillary function that gives new Node id
-newNode :: State Int Node
-newNode = do
-  i <- get
-  put (i + 1)
-  return $ Node i
-
-
--- Translates Regular expression to NFA --
-translate :: Regex -> State Int NFA
-
-translate Empty = do
-  node <- newNode
-  return $ mkNfa node [node] Map.empty
-
-translate (Lit c) = do
-  i <- newNode
-  t <- newNode
-  return $ mkNfa i [t] (Map.singleton i [(Transition c, t)])
-
-translate (Alt r1 r2) = do
-  a1 <- translate r1
-  a2 <- translate r2
-  start <- newNode
-  return $ mergeNFAs 
-    start 
-    (terminal a1 ++ terminal a2) 
-    [(start, [(Eps, initial a1), (Eps, initial a2)])]
-    [a1, a2]
-
-translate (Cat r1 r2) = do
-  a1 <- (translate r1)
-  a2 <- (translate r2)
-  return $ mergeNFAs 
-    (initial a1)
-    (terminal a2)
-    [(t, [(Eps, initial a2)]) | t <- terminal a1]
-    [a1, a2]
-
-translate (Star r) = do
-  a <- (translate r)
-  start <- newNode
-  return $ mergeNFAs
-    start
-    [start]
-    ([(t', [(Eps, start)]) | t' <- terminal a] ++ [(start, [(Eps, initial a)])])
-    [a]
-
-translate (Plus r) = translate (Cat r (Star r))
-
-translate (Range c1 c2)
-  | c1 > c2   = error "Invalid range expression"
-  | otherwise = translate $ foldl (Alt) (Lit c1) (map Lit [succ c1 .. c2])
-  
-
--- Takes a list of rules and translates them into a single NFA instance along with a terminal-to-token map
-translateMany :: [(Token, Regex)] -> (NFA, Map.Map Node Token)
-translateMany rules = fst $ runState buildNFA 0
-  where 
-    buildNFA = do
-      start <- newNode
-      nfas  <- mapM (translate . snd) rules  -- Translate regexes to NFAs
-
-      let 
-        newEdges        = Map.singleton start [(Eps, initial a) | a <- nfas]
-        allTerminals    = concatMap terminal nfas
-        terminalToToken = Map.fromList [(n, t) | (t, a) <- zip (map fst rules) nfas, n <- terminal a]
-        allTransitions  = foldl joinTs Map.empty (map transitions nfas)
-
-      return $ (mkNfa start allTerminals (allTransitions `joinTs` newEdges), terminalToToken)
 
 
 data Token = ID | NUM | OP deriving Show
@@ -158,11 +76,11 @@ main :: IO ()
 main = 
   let 
     rules =
-      [ (ID, Plus (Range 'a' 'b'))
-      , (NUM, Plus (Range '0' '1'))
-      , (OP, Alt (Lit '+') (Lit '-'))
+      [ (ID, Plus (Range 'a' 'z'))
+      , (NUM, Plus (Range '0' '9'))
+      , (OP, Alt (Lit '+') (Alt (Lit '-') (Alt (Lit '*') (Lit '/'))))
       ]
-    str = "a+b-10"
+    str = "id+20*x-abc/20"
     (nfa, t2t) = translateMany rules
   in do 
     Graph.showNFA nfa
--- src/Regex.hs
+++ src/Regex.hs
@@ -1,4 +1,8 @@
-module Regex (Regex(..)) where 
+module Regex (Regex(..), translateMany) where 
+
+import Control.Monad.State
+import qualified Data.Map as Map
+import NFA
 
 data Regex
     = Empty
@@ -9,3 +13,84 @@ data Regex
     | Plus Regex
     | Range Char Char 
   deriving Show
+
+
+-- Auxillary function that merges two NFA's and adds new edges
+mergeNFAs :: Node -> [Node] -> [(Node, [(Transition, Node)])] -> [NFA] -> NFA 
+mergeNFAs start terminals newEdges nfas = 
+ mkNfa 
+    start 
+    terminals 
+    (foldl joinTs Map.empty ((Map.fromList newEdges) : (map transitions nfas)))
+
+
+-- Auxillary function that gives new Node id
+newNode :: State Int Node
+newNode = do
+  i <- get
+  put (i + 1)
+  return $ Node i
+
+
+-- Translates Regular expression to NFA --
+translate :: Regex -> State Int NFA
+
+translate Empty = do
+  node <- newNode
+  return $ mkNfa node [node] Map.empty
+
+translate (Lit c) = do
+  i <- newNode
+  t <- newNode
+  return $ mkNfa i [t] (Map.singleton i [(Transition c, t)])
+
+translate (Alt r1 r2) = do
+  a1 <- translate r1
+  a2 <- translate r2
+  start <- newNode
+  return $ mergeNFAs 
+    start 
+    (terminal a1 ++ terminal a2) 
+    [(start, [(Eps, initial a1), (Eps, initial a2)])]
+    [a1, a2]
+
+translate (Cat r1 r2) = do
+  a1 <- (translate r1)
+  a2 <- (translate r2)
+  return $ mergeNFAs 
+    (initial a1)
+    (terminal a2)
+    [(t, [(Eps, initial a2)]) | t <- terminal a1]
+    [a1, a2]
+
+translate (Star r) = do
+  a <- (translate r)
+  start <- newNode
+  return $ mergeNFAs
+    start
+    [start]
+    ([(t', [(Eps, start)]) | t' <- terminal a] ++ [(start, [(Eps, initial a)])])
+    [a]
+
+translate (Plus r) = translate (Cat r (Star r))
+
+translate (Range c1 c2)
+  | c1 > c2   = error "Invalid range expression"
+  | otherwise = translate $ foldl (Alt) (Lit c1) (map Lit [succ c1 .. c2])
+
+
+-- Takes a list of rules and translates them into a single NFA instance along with a terminal-to-token map
+translateMany :: (Read t) => [(t, Regex)] -> (NFA, Map.Map Node t)
+translateMany rules = fst $ runState buildNFA 0
+  where 
+    buildNFA = do
+      start <- newNode
+      nfas  <- mapM (translate . snd) rules  -- Translate regexes to NFAs
+
+      let 
+        newEdges        = Map.singleton start [(Eps, initial a) | a <- nfas]
+        allTerminals    = concatMap terminal nfas
+        terminalToToken = Map.fromList [(n, t) | (t, a) <- zip (map fst rules) nfas, n <- terminal a]
+        allTransitions  = foldl joinTs Map.empty (map transitions nfas)
+
+      return $ (mkNfa start allTerminals (allTransitions `joinTs` newEdges), terminalToToken)
